1c6336a6b970a160063c2d983d91200d0088d4a8
yaswanthbandaru <yaswanthbandaru424@gamil.com>
2021-01-19 16:15:04 +0530

    Now adding conditionals to the problem

--- dice_roller.py
+++ dice_roller.py
@@ -5,5 +5,10 @@ dice_sum = 0
 for i in range(0, dice_rolls):
     roll = random.randint(1, 6)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    if roll ==1:
+        print(f'You rolled a {roll}! Critical Fail')
+    elif roll ==6:
+        print(f'You rolled a {roll}! Critical Succes!')
+    else:
+        print(f'You rolled a {roll}')
 print(f'You have rolled a total of {dice_sum}')
